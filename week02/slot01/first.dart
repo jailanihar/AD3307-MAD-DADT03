6e3826f8a6bd808e0497a95e86114610800bf643
Jailani Abdul Rahman <jailani.har@googlemail.com>
2025-07-28 09:22:41 +0800

    Week 02

---
+++ week02/slot01/first.dart
@@ -0,0 +1,7 @@
+void main() {
+  String name = 'Jailani';
+  // String concatenation
+  print('Hello, World! ' + name + ' is my name');
+  // String interpolation
+  print('Hello, World! $name is my name. ${name.toUpperCase()}');
+}
