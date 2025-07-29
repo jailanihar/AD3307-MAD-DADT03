4ff780dc4f66ada08beb11ae106cf22340e541a6
Jailani Abdul Rahman <jailani.har@googlemail.com>
2025-07-29 08:10:04 +0800

    Code after week 3 slot 1

--- week03/slot01/myfirstmobileapp/lib/main.dart
+++ week03/slot01/myfirstmobileapp/lib/main.dart
@@ -13,25 +13,50 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page Jailani'),
+      // home: const MyHomePage(title: 'Flutter Demo Home Page Jailani'),
+      home: const MyFirstPage(),
     );
+  }
+}
+
+class MyFirstPage extends StatelessWidget {
+  const MyFirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'My First Page',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: Column(
+        children: [
+          const Text('This is page body'),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Click Me'),
+          ),
+        ],
+      ),
+    );
+    // return Text(
+    //   'Hello, this is my first page!',
+    //   style: TextStyle(
+    //     color: Colors.cyan,
+    //     fontSize: 24,
+    //     fontWeight: FontWeight.bold,
+    //   ),
+    // );
   }
 }
 
